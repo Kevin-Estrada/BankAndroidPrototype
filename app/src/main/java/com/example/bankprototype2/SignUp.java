09dc5836f826d14c8dff5945e3a4c892484e9b0b
KevinEstrada12 <43767301+KevinEstrada12@users.noreply.github.com>
2019-12-19 03:50:50 -0600

    Clarified up some of the required texts in sign up page, and attempted to implement and fix the firebase firestone database

--- app/src/main/java/com/example/bankprototype2/SignUp.java
+++ app/src/main/java/com/example/bankprototype2/SignUp.java
@@ -12,10 +12,17 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignUp extends AppCompatActivity {
 
@@ -27,6 +34,8 @@ public class SignUp extends AppCompatActivity {
     private EditText confirmPassText;
     private Button registerAccount;
     private FirebaseAuth mAuth;
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,7 +62,7 @@ public class SignUp extends AppCompatActivity {
                     emailText.setError("Enter a valid email.");
                 } else if(passwordText.length() == 0 || passwordText.length() < 5){
                     passwordText.setError("Enter at least a 6 character password.");
-                } else if(confirmPassText != passwordText){
+                } else if(!(confirmPassText.getText().toString().contentEquals(passwordText.getText()))){
                     confirmPassText.setError("Passwords do not match.");
                 } else {
                     registerNewUser();
@@ -74,6 +83,7 @@ public class SignUp extends AppCompatActivity {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "createUserWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
+                            saveUserData();
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "createUserWithEmail:failure", task.getException());
@@ -86,4 +96,32 @@ public class SignUp extends AppCompatActivity {
 
 
     }
-}
+
+    private void saveUserData() {
+        String firstName = firtNameText.getText().toString();
+        String lastName = lastNameText.getText().toString();
+        String email = emailText.getText().toString();
+        String password = passwordText.getText().toString();
+        double balance = 0;
+
+        Map<String, Object> user = new HashMap<>();
+        user.put("First Name", firstName);
+        user.put("Last Name", lastName);
+        user.put("Email Address", email);
+        user.put("Password",password);
+        user.put("Balance",balance);
+
+        db.collection("Users").document(firstName)
+                .set(user)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            Log.d("User Register", "Document has been saved!");
+                        } else {
+                            Log.w("User Register","Document was not saved !",task.getException());
+                        }
+                    }
+                });
+                    }
+    }
